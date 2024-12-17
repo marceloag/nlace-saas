851539accb052d3f91692c60dc4604309b40dc79
Marcelo Javier Aguila <web@croop.cl>
2024-12-16 22:44:20 -0300

    Login Branding

--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,22 @@
-import { login, signup, loginWithGoogle } from "./actions";
+import { login, signup, loginWithGoogle } from './actions';
 
 export default function LoginPage() {
   return (
     <div>
       <form>
-        <div className='flex items-center justify-center h-screen dark:bg-gray-800'>
+        <div className="flex flex-col items-center justify-center h-screen dark:bg-gray-50 gap-8">
+          <img src="/nlace_black.svg" alt="Nlace Logo" className=" w-48" />
           <button
-            className='px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150'
+            className="bg-white px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150  border-b-8 border-solid border-gray-800 drop-shadow-md"
             formAction={loginWithGoogle}
           >
             <img
-              className='w-6 h-6'
-              src='https://www.svgrepo.com/show/475656/google-color.svg'
-              loading='lazy'
-              alt='google logo'
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              loading="lazy"
+              alt="google logo"
             />
-            <span>Inicia sesión con Google</span>
+            <span className="text-gray-800">Inicia sesión con Google</span>
           </button>
         </div>
       </form>
