489acfbbda558b22e2c388ed924b40baf09fbd34
Marcelo Javier Aguila <web@croop.cl>
2025-02-01 18:54:26 -0300

    Feat: Chat Switching

--- components/chat/Chat.js
+++ components/chat/Chat.js
@@ -2,13 +2,13 @@
 // TODO: Message history
 // TODO: Bug on avatar image on switching accounts
 import { useEffect, useState, useRef } from 'react';
-import { generarPauta, sendMessage } from './actions';
+import { sendMessage, getMessages, saveMessageSB } from './actions';
 import Loading from '../ui/Loading';
 import Markdown from 'react-markdown';
 import { motion } from 'motion/react';
 import { useAccount } from '@/context/AccountContext';
 
-function Chat({ userId, accountId }) {
+function Chat({ userId }) {
   const messagesEndRef = useRef(null);
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -21,30 +21,29 @@ function Chat({ userId, accountId }) {
   const avatar = currentAccount?.avatar;
 
   useEffect(() => {
-    // console.log(currentAccount);
-    const fetchPauta = async () => {
+    const fetchMessages = async () => {
       try {
-        const response = await generarPauta();
-        await setPauta(response.message.response.text);
-        setOpacity('opacity-0');
-        setTimeout(() => {
-          setLoadingPauta(false);
-        }, 500);
+        const messagesSB = await getMessages(userId, currentAccount?.id);
+        setMessages(messagesSB);
+        console.log(messages);
       } catch (error) {
-        console.error('Failed to fetch pauta:', error);
+        console.error('Failed to fetch messages:', error);
       }
     };
-    // TODO: Uncomment this line to fetch pauta
-    // fetchPauta();
+    fetchMessages();
   }, [currentAccount]);
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
+  const handleCommandEnter = (e) => {
+    if (e.key === 'Enter' && e.metaKey) {
+      handleSubmit(e);
+    }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // if (!prompt.trim() || isLoading) return;
 
     const userMessage = {
       id: Date.now().toString(),
@@ -55,6 +54,17 @@ function Chat({ userId, accountId }) {
     };
 
     setMessages((prev) => [...prev, userMessage]);
+    try {
+      const guardarMensaje = await saveMessageSB(
+        prompt.trim(),
+        'user',
+        currentAccount.id,
+        userId
+      );
+    } catch (error) {
+      console.error('Failed to save message:', error);
+    }
+
     setPrompt('');
     setIsLoading(true);
 
@@ -189,6 +199,7 @@ function Chat({ userId, accountId }) {
               placeholder="Escribe aquí lo que quieras ..."
               onChange={(e) => setPrompt(e.target.value)}
               value={prompt}
+              onKeyDown={handleCommandEnter}
             ></textarea>
             <a
               className="flex flex-col items-center justify-center bg-gradient-to-br from-[#ffc4f9] to-[#95a9fc] absolute w-10 h-10 text-white z-10 right-2 top-5 rounded-full cursor-pointer"
@@ -208,6 +219,38 @@ function Chat({ userId, accountId }) {
             </a>
           </div>
         </div>
+        <small className="text-center mt-1 text-gray-500 flex flex-row items-center justify-center gap-1">
+          Presiona
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#888888"
+              d="M17.5 3C15.57 3 14 4.57 14 6.5V8h-4V6.5C10 4.57 8.43 3 6.5 3S3 4.57 3 6.5S4.57 10 6.5 10H8v4H6.5C4.57 14 3 15.57 3 17.5S4.57 21 6.5 21s3.5-1.57 3.5-3.5V16h4v1.5c0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5s-1.57-3.5-3.5-3.5H16v-4h1.5c1.93 0 3.5-1.57 3.5-3.5S19.43 3 17.5 3M16 8V6.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5S18.33 8 17.5 8zM6.5 8C5.67 8 5 7.33 5 6.5S5.67 5 6.5 5S8 5.67 8 6.5V8zm3.5 6v-4h4v4zm7.5 5c-.83 0-1.5-.67-1.5-1.5V16h1.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5m-11 0c-.83 0-1.5-.67-1.5-1.5S5.67 16 6.5 16H8v1.5c0 .83-.67 1.5-1.5 1.5"
+            ></path>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 14 14"
+          >
+            <g
+              fill="none"
+              stroke="#888888"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <rect width="13" height="13" x=".5" y=".5" rx="1"></rect>
+              <path d="m5.5 10.5l-2-2l2-2"></path>
+              <path d="M3.5 8.5h5a1 1 0 0 0 1-1v-3"></path>
+            </g>
+          </svg>
+          para enviar
+        </small>
       </main>
     </>
   );
--- components/chat/actions.js
+++ components/chat/actions.js
@@ -1,4 +1,6 @@
 'use server';
+import { createClient } from '@/utils/supabase/server';
+const supabase = await createClient();
 
 export async function sendMessage(
   prompt,
@@ -23,7 +25,8 @@ export async function sendMessage(
     })
   });
   const result = await response.json();
-  console.log(result);
+  // console.log(result);
+  saveMessageSB(result.answer, 'ai', accountId, userId);
   return {
     message: result.answer
   };
@@ -45,4 +48,36 @@ export async function generarPauta() {
   return {
     message: result
   };
+}
+
+export async function getMessages(userId, accountId) {
+  if (accountId && accountId != 1) {
+    const session_id = `${userId}+${accountId}`;
+    const { data: mensajes, error } = await supabase
+      .from('mensajes_chat')
+      .select('*')
+      .eq('account_id', accountId)
+      .eq('user_id', userId);
+    if (error) throw error;
+    const formattedMessages = mensajes.map((mensaje) => {
+      return {
+        id: mensaje.id,
+        content: mensaje.content,
+        role: mensaje.role,
+        timestamp: new Date(mensaje.timestamp),
+        accountId: mensaje.account_id
+      };
+    });
+    return formattedMessages;
+  } else {
+    return [];
+  }
+}
+
+export async function saveMessageSB(content, role, accountId, userId) {
+  const { data, error } = await supabase
+    .from('mensajes_chat')
+    .insert({ content, role, account_id: accountId, user_id: userId });
+  if (error) throw error;
+  return data;
 }
