d1da1e80131487c9b78625c35600d2f5ca2729d5
Marcelo Javier Aguila <web@croop.cl>
2025-02-01 18:55:26 -0300

    Fix: Class in Loading component

--- components/ui/Loading.jsx
+++ components/ui/Loading.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Loading() {
   return (
-    <section class="dots-container">
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
     </section>
   );
 }
