61706b438be3baf5e2082f4a9630b98cb074e824
Marcelo Javier Aguila <web@croop.cl>
2025-02-17 10:50:13 -0300

    Updated App name and enabled Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,11 @@
 import localFont from 'next/font/local';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Nlace Saas Alpha',
-  description: 'Marcelo Aguila para Nlace'
+  title: 'Nlace Ai Agent Studio',
+  description:
+    'Aplicación de creación de agentes de IA personalizados para clientes de Nlace.com'
 };
 
 export default function RootLayout({ children }) {
