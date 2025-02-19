badbf1d259b20092f93f43ab6bf819ebfa2bf1ea
Marcelo Javier Aguila <web@croop.cl>
2025-02-19 10:03:11 -0300

    First development push

--- app/login/actions.js
+++ app/login/actions.js
@@ -1,26 +1,26 @@
-"use server";
+'use server';
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from '@/utils/supabase/server';
 
 export async function login(formData) {
   const supabase = await createClient();
 
   const data = {
-    email: formData.get("email"),
-    password: formData.get("password"),
+    email: formData.get('email'),
+    password: formData.get('password')
   };
 
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
-    redirect("/error");
+    redirect('/error');
   }
 
-  revalidatePath("/", "layout");
-  redirect("/dashboard");
+  revalidatePath('/', 'layout');
+  redirect('/dashboard');
 }
 
 export async function signup(formData) {
@@ -29,27 +29,27 @@ export async function signup(formData) {
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get("email"),
-    password: formData.get("password"),
+    email: formData.get('email'),
+    password: formData.get('password')
   };
 
   const { error } = await supabase.auth.signUp(data);
 
   if (error) {
-    redirect("/error");
+    redirect('/error');
   }
 
-  revalidatePath("/", "layout");
-  redirect("/");
+  revalidatePath('/', 'layout');
+  redirect('/');
 }
 
 export async function loginWithGoogle() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
+    provider: 'google',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
-    },
+      redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`
+    }
   });
 
   if (data.url) {
