850e83ddef6a42c4daa298fcb4a9e3a7377827f9
Marcelo Javier Aguila <web@croop.cl>
2025-02-26 16:10:16 -0300

    Model Selection Logic

--- components/icons/Icons.js
+++ components/icons/Icons.js
@@ -441,7 +441,6 @@ export const Anthropic = (props) => (
       lineHeight: 1
     }}
     viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
     {...props}
