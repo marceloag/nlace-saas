5cc4eaa48fa8828806da44d1da4a4faee4ae187f
Marcelo Javier Aguila <web@croop.cl>
2025-02-26 16:10:16 -0300

    Model Selection Logic

--- components/chat/Chat.js
+++ components/chat/Chat.js
@@ -110,6 +110,7 @@ function Chat({ userId }) {
 
   return (
     <>
+      {/* TODO: */}
       <header className="bg-transparent m-0 p-0 after:via-gray-50 after:via-40% w-full after:absolute after:w-full after:z-50 after:pb-10 after:bg-gradient-to-b after:flex  after:from-gray-50 after:to-transparent">
         <h1 className="text-3xl font-thin">
           👋 Bienvenido al agente de : {currentAccount?.nombre}
