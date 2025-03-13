4db9d7abee455d10b02b1f26564666bf12e79303
Marcelo Javier Aguila <web@croop.cl>
2025-03-13 17:05:41 -0300

    Real full debugged chat

--- components/chat/newChat.js
+++ components/chat/newChat.js
@@ -4,7 +4,7 @@ import { useEffect, useRef } from 'react';
 import { useAccount } from '@/context/AccountContext';
 import ChatInput from '@/components/chat/ui/ChatInput';
 import ChatMessages from '@/components/chat/ui/ChatMessages';
-import SocialPostItem from '@components/ui/social-post-item';
+import { useChat } from '@ai-sdk/react';
 
 function NewChat({ userId }) {
   const { messages, input, handleInputChange, handleSubmit, status } = useChat({
@@ -13,14 +13,6 @@ function NewChat({ userId }) {
   const { currentAccount } = useAccount();
   const avatar = currentAccount?.avatar;
   const prevAccountRef = useRef();
-  const post = {
-    id: '2',
-    content:
-      'Estamos emocionados de anunciar nuestra colaboración con @empresa. Juntos crearemos experiencias increíbles para nuestros usuarios.',
-    platform: 'linkedin',
-    date: new Date(Date.now() + 172800000), // Day after tomorrow
-    time: '14:30'
-  };
 
   useEffect(() => {
     if (prevAccountRef.current?.id === currentAccount?.id) {
@@ -33,15 +25,6 @@ function NewChat({ userId }) {
   return (
     <>
       <main className="w-full flex flex-1 flex-col bg-white px-4">
-        <SocialPostItem
-          key={post.id}
-          id={post.id}
-          content={post.content}
-          platform={post.platform}
-          date={post.date}
-          time={post.time}
-          onSave={handleSavePost}
-        />
         <ChatMessages
           messages={messages}
           status={status}
