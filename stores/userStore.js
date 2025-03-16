994ddebf1360596735bbf2b0cb7367adf1a8e370
Marcelo Javier Aguila <web@croop.cl>
2025-03-16 16:56:16 -0300

    Removed Supabase from store

--- stores/userStore.js
+++ stores/userStore.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-// import { supabase } from '@/lib/supabaseClient';
 
 export const useUserStore = create()(
   persist(
