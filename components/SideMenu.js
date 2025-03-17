ef964ea2920fb008761cc6163ab981bf3ba6ff7a
Marcelo Javier Aguila <web@croop.cl>
2025-03-17 09:56:09 -0300

    Fixed auth flow, and dashboard elements load

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -31,9 +31,10 @@ function SideMenu() {
   };
   return (
     <aside className="w-14 shadow-md h-screen flex flex-col gap-2 py-2 px-2 items-center justify-between bg-white fixed left-0 top-0 z-50">
-      <figure className="rounded-full overflow-hidden border-[2px] border-solid border-pink-100 w-full">
+      <figure className="rounded-full overflow-hidden border-[2px] border-solid border-pink-100 w-full aspect-square">
+        {}
         <img
-          src={userData?.user.user_metadata.picture}
+          src={userData?.user.user_metadata.picture ?? '/avatar-default.webp'}
           className="aspect-square w-full"
           alt="Avatar"
         />
