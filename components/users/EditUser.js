7f7bf8d908654ed37b0f882f5ac2d916256c75ef
Marcelo Javier Aguila <web@croop.cl>
2025-03-16 22:21:17 -0300

    Visual Fixes

--- components/users/EditUser.js
+++ components/users/EditUser.js
@@ -5,7 +5,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
-import { Alert, AlertCircle, AlertDescription } from '@/components/ui/alert';
+import { Alert, AlertDescription } from '@/components/ui/alert';
+import { AlertCircle } from 'lucide-react';
 import {
   updateAccount,
   getAccounts,
