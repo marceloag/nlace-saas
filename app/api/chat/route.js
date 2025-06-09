afc53e78b1f5d71f513ebeca29fdbe9662eab10d
Marcelo Javier Aguila <web@croop.cl>
2025-06-09 12:21:40 -0300

    Fixed: Removed get weather tool

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -72,7 +72,6 @@ export async function POST(req) {
       getKnowledgeBase,
       generatePosts,
       generateChart,
-      get_Weather,
       ...mcpToolsDice,
       ...mcpToolsTrello,
       ...firecrawlTools
