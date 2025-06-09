b8b817f72ace0467b8edbea62c10824e8cac7527
Marcelo Javier Aguila <web@croop.cl>
2025-06-09 10:22:09 -0300

    Fixed missing firecrawl mcp & weather

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,19 +1,16 @@
-import { openai } from '@ai-sdk/openai';
-import { anthropic } from '@ai-sdk/anthropic';
-import { streamText } from 'ai';
-import { getKnowledgeBase } from '@/lib/ai/tools/getKnowledgeBase';
-import { getWeather } from '@/lib/ai/tools/getWeather';
-import { generatePosts } from '@/lib/ai/tools/generatePosts';
-import { generateChart } from '@/lib/ai/tools/generateChart';
-import { generateImages } from '@/lib/ai/tools/generateImage';
-import { systemPrompt } from '@/lib/constants/prompts';
+import { openai } from '@ai-sdk/openai'
+import { anthropic } from '@ai-sdk/anthropic'
+import { streamText } from 'ai'
+import { getKnowledgeBase } from '@/lib/ai/tools/getKnowledgeBase'
+import { getWeather } from '@/lib/ai/tools/getWeather'
+import { generatePosts } from '@/lib/ai/tools/generatePosts'
+import { generateChart } from '@/lib/ai/tools/generateChart'
+import { systemPrompt } from '@/lib/constants/prompts'
 // MCP
-import { experimental_createMCPClient as createMCPClient } from 'ai';
-import { Experimental_StdioMCPTransport as StdioMCPTransport } from 'ai/mcp-stdio';
-import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp';
-// SUPABASE
+import { experimental_createMCPClient as createMCPClient } from 'ai'
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp'
 
-export const maxDuration = 45;
+export const maxDuration = 45
 
 export async function POST(req) {
   const {
@@ -23,48 +20,46 @@ export async function POST(req) {
     promptAgente,
     accountNombre,
     conversationId
-  } = await req.json();
+  } = await req.json()
 
-  // MCP
+  // MCP FIRECRAWL
+  const firecrawlMCP = await createMCPClient({
+    transport: {
+      type: 'sse',
+      url: `https://mcp.firecrawl.dev/${process.env.FIRECRAWL_MCP_KEY}/sse`
+    }
+  })
 
-  // const mcpClient = await createMCPClient({
-  //   transport: new StdioMCPTransport({
-  //     command: 'npx',
-  //     args: ['-y', '@enconvo/trello-mcp-server']
-  //   })
-  // })
+  const firecrawlTools = await firecrawlMCP.tools()
 
-  const mcpUrl = `${process.env.NEXT_PUBLIC_URL}/api/mcp`;
+  const mcpUrl = `${process.env.NEXT_PUBLIC_URL}/api/mcp`
 
   const customClient = await createMCPClient({
     transport: new StreamableHTTPClientTransport(mcpUrl, {
       sessionId: 'session_123'
     })
-  });
+  })
 
-  const mcpToolsDice = await customClient.tools();
+  const mcpToolsDice = await customClient.tools()
 
   // MCP TRELLO
-  const mcpTrelloUrl = `${process.env.NEXT_PUBLIC_URL}/api/mcp-trello/mcp`;
-
+  const mcpTrelloUrl = `${process.env.NEXT_PUBLIC_URL}/api/mcp-trello/mcp`
   const customClientTrello = await createMCPClient({
     transport: new StreamableHTTPClientTransport(mcpTrelloUrl)
-  });
-
-  const mcpToolsTrello = await customClientTrello.tools();
-
+  })
+  const mcpToolsTrello = await customClientTrello.tools()
   // END MCP
 
   const messagesHavePDF = messages.some((message) =>
     message.experimental_attachments?.some(
       (a) => a.contentType === 'application/pdf'
     )
-  );
+  )
 
   const userMessage = messages
     .filter((msg) => msg.role === 'user')
-    .pop()?.content;
-  const system = systemPrompt(userId, accountId, promptAgente, accountNombre);
+    .pop()?.content
+  const system = systemPrompt(userId, accountId, promptAgente, accountNombre)
   // const system = `You are a friendly assistant! Keep your responses concise and helpful.`;
 
   const result = streamText({
@@ -77,23 +72,25 @@ export async function POST(req) {
       getKnowledgeBase,
       generatePosts,
       generateChart,
+      get_Weather,
       ...mcpToolsDice,
-      ...mcpToolsTrello
+      ...mcpToolsTrello,
+      ...firecrawlTools
     },
     toolCallStreaming: true,
     maxSteps: 5,
     experimental_telemetry: { isEnabled: true },
     onFinish: async ({ response }) => {
-      console.log(response.usage);
-      console.log(JSON.stringify(response, null, 2));
+      console.log(response.usage)
+      console.log(JSON.stringify(response, null, 2))
       // MCP
-      await customClient.close();
-      await customClientTrello.close();
+      await customClient.close()
+      await customClientTrello.close()
     },
     onToolCall(tool, input) {
-      console.log('Tool:', tool.name, 'Input:', input);
+      console.log('Tool:', tool.name, 'Input:', input)
     }
-  });
+  })
 
-  return result.toDataStreamResponse();
+  return result.toDataStreamResponse()
 }
